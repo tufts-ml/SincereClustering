10c12c8e6128b54cd9e543fcdcfb6e2fb4d8a040
PatrickFeeney <patrickfeeney1225@gmail.com>
2023-12-06 10:33:46 -0500

    Debug Script

---
+++ scripts/run_debug_cifar10.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -p ccgpu,gpu
+#SBATCH -t 0-1:0:0
+#SBATCH --gres=gpu:a100:1
+#SBATCH -c 9
+#SBATCH -o ./slurm/output.%a.out
+
+set -e
+set -x
+
+CUDA_VISIBLE_DEVICES=0 pipenv run python -m cProfile -o stats.out train.py \
+    --dataset_name 'cifar10' \
+    --batch_size 128 \
+    --grad_from_block 11 \
+    --epochs 200 \
+    --num_workers 8 \
+    --use_ssb_splits \
+    --sup_weight 0.35 \
+    --weight_decay 5e-5 \
+    --transform 'imagenet' \
+    --lr 0.1 \
+    --eval_funcs 'v2' \
+    --warmup_teacher_temp 0.07 \
+    --teacher_temp 0.04 \
+    --warmup_teacher_temp_epochs 30 \
+    --memax_weight 1 \
+    --exp_name cifar10_simgcd
